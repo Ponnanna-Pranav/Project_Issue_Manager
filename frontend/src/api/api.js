507bbd6f1f4a821fa569fe26b72ac3079b51cd4d
Ponnanna-Pranav <117303087+Ponnanna-Pranav@users.noreply.github.com>
2026-02-07 15:42:22 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://project-issue-manager.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
